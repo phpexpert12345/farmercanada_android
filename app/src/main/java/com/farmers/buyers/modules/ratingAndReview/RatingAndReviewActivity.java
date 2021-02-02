9658199a86b2ef19cc178502cfb0ea5c29f5de18
pritamsnkadiya <pritamsnkadiya@gmail.com>
2021-02-02 19:21:42 +0530

    design branch-> 4 screen

---
+++ app/src/main/java/com/farmers/buyers/modules/ratingAndReview/RatingAndReviewActivity.java
@@ -0,0 +1,95 @@
+package com.farmers.buyers.modules.ratingAndReview;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.farmers.buyers.R;
+import com.farmers.buyers.core.BaseActivity;
+import com.farmers.buyers.modules.ratingAndReview.fragment.ReviewFragment;
+import com.farmers.buyers.modules.ratingAndReview.fragment.ReviewedFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RatingAndReviewActivity extends BaseActivity {
+
+    public TabLayout tabLayout;
+    public ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating_and_review);
+
+        setupToolbar(new ToolbarConfig("Rating And Review", true, new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        }, false, new ToolbarMenuConfig(R.drawable.ic_notification, new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        })));
+
+        initView();
+    }
+
+    private void initView() {
+
+        viewPager = findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ReviewFragment(), "Review");
+        adapter.addFragment(new ReviewedFragment(), "Reviewed");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    @Override
+    public Boolean showToolbar() {
+        return true;
+    }
+}
