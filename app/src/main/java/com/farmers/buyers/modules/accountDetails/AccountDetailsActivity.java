9e7ae6d734b922bf7170f6e376bbbe3e484e584e
pritamsnkadiya <pritamsnkadiya@gmail.com>
2021-02-04 19:14:34 +0530

    Dailog and design related work done

--- app/src/main/java/com/farmers/buyers/modules/accountDetails/AccountDetailsActivity.java
+++ app/src/main/java/com/farmers/buyers/modules/accountDetails/AccountDetailsActivity.java
@@ -1,13 +1,17 @@
 package com.farmers.buyers.modules.accountDetails;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
 import com.farmers.buyers.R;
 import com.farmers.buyers.core.BaseActivity;
 import com.farmers.buyers.modules.changePassword.ChangePasswordActivity;
+import com.farmers.buyers.modules.profile.EditProfileActivity;
 
 public class AccountDetailsActivity extends BaseActivity implements View.OnClickListener {
 
@@ -31,6 +35,8 @@ public class AccountDetailsActivity extends BaseActivity implements View.OnClick
         })));
 
         init();
+
+        order_track_dialog(this);
     }
 
     private void init() {
@@ -47,5 +53,38 @@ public class AccountDetailsActivity extends BaseActivity implements View.OnClick
     @Override
     public void onClick(View view) {
         startActivity(new Intent(AccountDetailsActivity.this, ChangePasswordActivity.class));
+    }
+
+    private void write_about_farm_dialog(Activity activity) {
+
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.write_about_farm_dialog, null);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity, R.style.NewDialog);
+        alertDialogBuilder.setView(promptsView);
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void notify_me_switches_dialog(Activity activity) {
+
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.notify_me_switches_dialog, null);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity, R.style.NewDialog);
+        alertDialogBuilder.setView(promptsView);
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void order_track_dialog(Activity activity) {
+
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.order_track_dialog, null);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity, R.style.NewDialog);
+        alertDialogBuilder.setView(promptsView);
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
